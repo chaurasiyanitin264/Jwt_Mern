c89103e6ce4a6327f26e2e7b6950bc00e72d32dd
Nitin Chaurasiya <chaurasiyanitin264@gmail.com>
2025-02-10 15:51:57 +0530

    JWT_Mern

--- client/src/component/TopMenu.jsx
+++ client/src/component/TopMenu.jsx
@@ -2,20 +2,56 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const TopMenu = () => {
-    return (
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/home");
+  };
+
+  return (
+    <>
+      {localStorage.getItem("username") ? (
         <>
-            <Navbar style={{backgroundColor:"#94d3e9"}} expand="lg" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home">JWT</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="registration">Registration</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+          <Navbar bg="primary" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-nav" />
+              <Navbar.Collapse id="navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="home">Home</Nav.Link>
+                </Nav>
+                <div className="ms-auto text-white">
+                  Welcome: {localStorage.getItem("username")} <br />
+                  Email: {localStorage.getItem("useremail")} <br />
+                  <button onClick={logout} className="btn btn-light">Logout</button>
+                </div>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </>
-    )
-}
+      ) : (
+        <>
+          <Navbar bg="primary" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-nav" />
+              <Navbar.Collapse id="navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="home">Home</Nav.Link>
+                  <Nav.Link as={Link} to="login">Login</Nav.Link>
+                  <Nav.Link as={Link} to="registration">Registration</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </>
+      )}
+    </>
+  );
+};
+
 export default TopMenu;
